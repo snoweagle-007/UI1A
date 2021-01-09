66656a3e2f90778059adcacacde15b4ef65ef2f0
snoweagle-007 <67202369+snoweagle-007@users.noreply.github.com>
2021-01-09 10:58:46 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:image_up/home.dart';
+
+void main() => runApp(MaterialApp(
+      title: 'Financial App',
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
